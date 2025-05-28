34e664653431e0ddba12ac82e052aedd8dcda036
Marios Tsigkas <mariosh346@gmail.com>
2025-05-28 21:22:14 +0300

    Add Providers component for React Query and update RootLayout structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
-import {ReactNode} from 'react';
+import { ReactNode } from 'react';
+import Providers from './providers';
 
 type Props = {
   children: ReactNode;
@@ -6,6 +7,14 @@ type Props = {
 
 // Since we have a `not-found.tsx` page on the root, a layout file
 // is required, even if it's just passing children through.
-export default function RootLayout({children}: Props) {
-  return children;
+export default function RootLayout({ children }: Props) {
+  return (
+    <html>
+      <body>
+        <Providers>
+          {children}
+        </Providers>
+      </body>
+    </html>
+  );
 }
