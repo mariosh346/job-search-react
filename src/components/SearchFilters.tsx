4616db072b7de33b41d61c4bd33e2f7ed7ac2aaf
Marios Tsigkas <mariosh346@gmail.com>
2025-05-29 17:13:53 +0300

    Add SearchFilters component for filtering job listings

---
+++ src/components/SearchFilters.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import { useTranslations } from 'next-intl';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useFiltersQuery } from '@/hooks/useJobsQuery';
+import { Input } from '@mui/material';
+import { Filter } from './Filter';
+
+export default function SearchFilters() {
+    const t = useTranslations('Filters');
+    const router = useRouter();
+    const searchParams = useSearchParams();
+
+    const { data: filters } = useFiltersQuery();
+
+    const handleFilterChange = (type: string, value: unknown) => {
+        const params = new URLSearchParams(searchParams.toString());
+        params.set(type, String(value));
+        router.push(`?${params.toString()}`);
+    };
+    const location = searchParams.get('location') || '';
+    const category = searchParams.get('category') || '';
+
+    return (
+        <div className="my-8 flex gap-4">
+            <Filter
+                type="location"
+                value={location}
+                options={filters?.locations}
+                onChange={(value) => handleFilterChange('location', value)}
+            />
+
+            <Filter
+                type="category"
+                value={category}
+                options={filters?.categories}
+                onChange={(value) => handleFilterChange('category', value)}
+            />
+
+            <Input
+                type="search"
+                placeholder={t('searchPlaceholder')}
+                className="p-2"
+                onChange={(e) => handleFilterChange('q', e.target.value)}
+            />
+        </div>
+    );
+}
