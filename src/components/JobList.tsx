53d153700d3507bccf6bc947e28d019e38e61e8f
Marios Tsigkas <mariosh346@gmail.com>
2025-05-29 18:02:33 +0300

    Add JobList component to display job listings with details

---
+++ src/components/JobList.tsx
@@ -0,0 +1,43 @@
+import { useTranslations } from 'next-intl';
+import { Jobs } from '@/schemas/jobsSchema';
+import { Typography } from '@mui/material';
+
+interface JobListProps {
+    jobs: Jobs | undefined;
+}
+
+export default function JobList({ jobs }: JobListProps) {
+    const t = useTranslations('Jobs');
+
+    if (!jobs?.results?.length) {
+        return (
+            <div>
+                {t('No jobs found')}
+            </div>
+        )
+    }
+
+    return (
+        <div className="gap-4">
+            {jobs?.results.map((job) => (
+                <article
+                    key={job.id}
+                    className="p-4 border rounded-lg mb-6"
+                >
+                    <Typography variant="h4" component="h2">
+                        {job.title}
+                    </Typography>
+                    <div className="mt-2">
+                        <p>{job.company}</p>
+                        <p>{job.location}</p>
+                    </div>
+                    <div className="mt-4">
+                        <span className="p-2 text-sm mr-2 bg-gray-100 rounded-md">
+                            {job.category}
+                        </span>
+                    </div>
+                </article>
+            ))}
+        </div>
+    );
+}
